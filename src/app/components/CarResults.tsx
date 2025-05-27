f35b6a09e8e2d4fdd1a4b883e5eec88e8da6615f
workProject085 <workProject085@gmail.com>
2025-05-28 02:03:33 +0300

    commit

--- src/app/components/CarResults.tsx
+++ src/app/components/CarResults.tsx
@@ -106,7 +106,7 @@ export default function CarResults({ results }: Props) {
               className="border rounded-xl overflow-hidden shadow-sm hover:shadow-md transition p-4 grid grid-cols-1 sm:grid-cols-[150px_1fr_120px_2fr] gap-4 items-start"
             >
               {/* 1. Фото */}
-              <div className="sm:w-full md:w-[150px] h-[100px] flex-shrink-0">
+              <div className="sm:w-full md:w-[150px] sm:h-[150px] md:h-[100px] flex-shrink-0">
                 <img
                   src={car.image}
                   alt={car.title}
@@ -162,9 +162,9 @@ export default function CarResults({ results }: Props) {
                         console.warn("sellerId отсутствует для машины", car);
                       }
                     }}
-                    className="mt-1 text-xs px-2 py-1 rounded bg-green-200 text-green-900"
+                    className="mt-1 text-xs px-2 py-1 rounded bg-green-200 text-green-900 pointer"
                   >
-                    {car.sellerCarCount}
+                    Авто продавца{" "}
                   </button>
                 </div>
               </div>
