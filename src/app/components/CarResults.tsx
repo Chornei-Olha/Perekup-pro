aadfbbab219978d4220855b32d6538d302ac2867
workProject085 <workProject085@gmail.com>
2025-05-27 11:59:49 +0300

    commit

--- src/app/components/CarResults.tsx
+++ src/app/components/CarResults.tsx
@@ -74,7 +74,8 @@ export default function CarResults({ results }: Props) {
                     </h3>
                     <p className="text-sm text-white">
                       {car.year} • {car.mileage.toLocaleString()} км •{" "}
-                      {getGearboxLabel(car.gearbox)} • {car.fuel}
+                      {getGearboxLabel(car.gearbox)} • {car.engineVolume} •{" "}
+                      {car.fuel}
                     </p>
                     <p className="text-sm text-white">{car.city}</p>
                     <p className="text-sm text-gray-200">
