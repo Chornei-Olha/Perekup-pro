27f0b3dd078cc60a23e5f27580be0366636a6317
DinisZabrodsky <190386070+Denis-WebDev@users.noreply.github.com>
2025-08-08 21:22:47 +0300

    fix cors bug

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,7 @@ const BASE_URL =
 
 export async function getBrands(): Promise<Brand[]> {
   try {
-    const res = await fetch(`${BASE_URL}/brands/`);
+    const res = await fetch(`${BASE_URL}/brands/index.php`);
 
     if (!res.ok) {
       throw new Error(
@@ -41,7 +41,7 @@ export async function getRegions(): Promise<Region[]> {
 
 export async function getModels(brands: number[]): Promise<Model[]> {
   try {
-    const res = await fetch(`${BASE_URL}/models/`, {
+    const res = await fetch(`${BASE_URL}/models/index.php`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ brands }),
